31b255e857b21edbadf80f06cac920e78e87ed30
Elgun Mehdi <elgun88@gmail.com>
2023-10-25 12:35:33 -0600

    Create basic calc operations in js

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let firstNumber = 1;
+let secondNumber = 2;
+let operator = '+';
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const operate = function (operator, firstNum, secondNum) {
+  switch (operator) {
+    case '+':
+      return add(firstNum, secondNum);
+    case '-':
+      return subtract(firstNum, secondNum);
+    case '*':
+      return multiply(firstNum, secondNum);
+    case '/':
+      return divide(firstNum, secondNum);
+    default:
+      return 'Please use a valid operator';
+  }
+};
