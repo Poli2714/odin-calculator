0f725f6873a8f38fce54928845d49cfff77ec566
Elgun Mehdi <elgun88@gmail.com>
2023-10-25 19:21:07 -0600

    Add simple functionality to calculator

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 'use strict';
 
-let firstNumber = 1;
+const calculator = document.querySelector('.calculator');
+const result = document.querySelector('.result');
+
+let firstNumber = 0;
 let secondNumber = 2;
 let operator = '+';
+let firstNumDefined = false;
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -23,3 +27,35 @@ const operate = function (operator, firstNum, secondNum) {
       return 'Please use a valid operator';
   }
 };
+
+const displayNumbers = function (textNum) {};
+
+let displayNum = '';
+const calcObj = {};
+
+calculator.addEventListener('click', function (e) {
+  const target = e.target;
+  if (!target.classList.contains('btn')) return;
+
+  if (target.classList.contains('number')) {
+    if (result.textContent === '0' && target.textContent === '0') return;
+    displayNum += target.textContent;
+    result.textContent = displayNum;
+  }
+
+  if (target.classList.contains('clear')) {
+    result.textContent = '0';
+    displayNum = [];
+  }
+
+  if (target.classList.contains('operator')) {
+    firstNumber = +result.textContent;
+    operator = target.textContent;
+    displayNum = '';
+  }
+
+  if (target.classList.contains('equal')) {
+    secondNumber = +result.textContent;
+    result.textContent = operate(operator, firstNumber, secondNumber);
+  }
+});
